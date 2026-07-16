267e41b8fbe3f5aaa08b20c6a878680529424034
Aven Martin <a.c.martin2696@gmail.com>
2026-07-16 16:41:28 -0500

    Create queries.sql

---
+++ 01-sql/queries.sql
@@ -0,0 +1,9 @@
+/*
+==========================================
+SQL Practice
+Author: Aven
+Description:
+A collection of SQL practice queries,
+examples, and exercises.
+==========================================
+*/
